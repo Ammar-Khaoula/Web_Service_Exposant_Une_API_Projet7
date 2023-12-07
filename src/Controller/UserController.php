e6b348f608f4c328482ba97563b46780ea5309d6
Ammar Khaoula <khaoulabha1991@gmail.com>
2023-12-07 15:29:17 +0100

    create user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,13 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\CustomerRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class UserController extends AbstractController
 {
@@ -40,5 +44,24 @@ class UserController extends AbstractController
         $em->remove($user);
         $em->flush();
         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+    #[Route('/api/users', name: 'addUser', methods:['POST'])]
+    public function createUser(Request $request, EntityManagerInterface $em, SerializerInterface $serializer,UrlGeneratorInterface $urlGeneratoer, CustomerRepository $customerRepo): JsonResponse
+    {
+        $data = $request->getContent();
+        $user = $serializer->deserialize($data, User::class, 'json');
+        
+        $em->persist($user); 
+
+        $content = $request->toArray();
+        $idCustomer = $content['idCustomer'];
+        $user->setCustomer($customerRepo->find($idCustomer));
+        $em->flush();
+
+        $jsonUser = $serializer->serialize($user, 'json', [ 'groups' => "getUsers"]);
+        $location = $urlGeneratoer->generate('user', ['id' => $user->getId()],  UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse($jsonUser, Response::HTTP_CREATED, ["location" => $location], true);
     }
 }
